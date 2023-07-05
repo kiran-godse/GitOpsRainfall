86c62bf6064e42e21eed5b1051de72c7e066b527
Kiran P Godse <kpgodse@gmail.com>
2023-07-05 12:10:32 +0530

    updted scode

--- validate-recipe.js
+++ validate-recipe.js
@@ -1,5 +1,21 @@
 const Ajv = require('ajv');
 const fs = require('fs');
 
-const schemaPath = './schema/recipe-schema.json';
-const recipePath = './recipes/cmake.recipe';
+async function validateSchema() {
+  const ajv = new Ajv();
+  const schema = JSON.parse(fs.readFileSync('./schema/recipe-schema.json', 'utf8')); // Update the path to your JSON schema
+
+  const validate = ajv.compile(schema);
+  const data = JSON.parse(fs.readFileSync('./recipes/cmake.recipe', 'utf8')); // Update the path to your JSON data
+
+  const valid = validate(data);
+  if (!valid) {
+    console.log('Schema validation failed.');
+    console.log(validate.errors);
+    process.exit(1);
+  } else {
+    console.log('Schema validation succeeded.');
+  }
+}
+
+validateSchema();
