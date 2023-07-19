48aeecf2bed942602cd8f7b1c30011d8dac4a63f
kiran-godse <82302395+kiran-godse@users.noreply.github.com>
2023-07-19 23:55:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const ajvKeywords = require('ajv-keywords');
 const schema = require('./.schema/recipe.json');
 const recipeData = require('./recipes/cmake.json');
 
-//Remove the $schema keyword from the schema
+// Remove the $schema keyword from the schema
 delete schema['$schema'];
+
+// Fix the schema object to remove unsupported custom keyword "cname"
+delete schema.properties.package.properties.name.cname;
+
+// Fix the schema object to set the "uniqueItems" keyword to boolean true
+schema.properties.package.properties.platforms.uniqueItems = true;
 
 const ajv = new Ajv.default({ allErrors: true });
 ajvKeywords(ajv, ['regexp']);
@@ -30,5 +36,3 @@ function readRecipe(data) {
         console.log('Substrate data:', data.substrate);
     }
 }
-
-
